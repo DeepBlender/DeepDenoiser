fdc8f4335e39bf16310ffaa055a4671bddef26b9
Deep Blender <deepblender@gmail.com>
2018-04-12 23:47:29 +0200

    - TFRecordsCreator is still work in progress! - Made the code a little bit clearer. - Added file compression.

--- TensorFlow/TFRecordsCreator.py
+++ TensorFlow/TFRecordsCreator.py
@@ -21,41 +21,55 @@ parser = argparse.ArgumentParser(description='Create tfrecords files for the Dee
 parser.add_argument('json_filename', help='The json specifying all the relevant details.')
 
 
-def mode_name_to_render_directories(parsed_json):
-  result = {}
+class TFRecordsCreator:
 
-  base_render_directories = parsed_json['base_render_directories']
-  mode_name_to_mode_settings = parsed_json['modes']
-  
-  source = parsed_json['source']
-  source_samples_per_pixel = source['samples_per_pixel']
-  source_render_passes_usage = RenderPassesUsage()
-  source_render_passes_usage.__dict__ = source['features']
-  
-  target = parsed_json['target']
-  target_samples_per_pixel = target['samples_per_pixel']
-  target_render_passes_usage = RenderPassesUsage()
-  target_render_passes_usage.__dict__ = target['features']
-  
-  for mode_name in mode_name_to_mode_settings:
-    mode_settings = mode_name_to_mode_settings[mode_name]
-    relative_render_directories = mode_settings['render_directories']
-    render_directories_result = []
-    for relative_directory in relative_render_directories:
-      render_directories = RenderDirectories(os.path.join(base_render_directories, relative_directory))
+  # TODO: Add log file functionality from previous version. (DeepBlender)
+  # TODO: Add statistics from previous version. (DeepBlender)
+
+  def __init__(
+      self, name, base_tfrecords_directory, base_render_directories, render_directories,
+      source_samples_per_pixel, source_render_passes_usage,
+      target_samples_per_pixel, target_render_passes_usage,
+      tiles_height_width, tiles_per_tfrecords):
+    self.name = name
+    self.base_tfrecords_directory = base_tfrecords_directory
+    self.source_samples_per_pixel = source_samples_per_pixel
+    self.source_render_passes_usage = source_render_passes_usage
+    self.target_samples_per_pixel = target_samples_per_pixel
+    self.target_render_passes_usage = target_render_passes_usage
+    self.tiles_height_width = tiles_height_width
+    self.tiles_per_tfrecords = tiles_per_tfrecords
+    
+    # REMARK: I couldn't find less confusing variable names, sorry about that. (DeepBlender)
+    self.render_directories = []
+    for relative_directories in render_directories:
+      new_render_directories = RenderDirectories(os.path.join(base_render_directories, relative_directories))
       
       # TODO: Certainly not the best way to perform the validity checks. (DeepBlender)
-      assert render_directories.required_files_exist(source_samples_per_pixel, source_render_passes_usage)
+      assert new_render_directories.required_files_exist(self.source_samples_per_pixel, self.source_render_passes_usage)
       
       samples_per_pixel = target_samples_per_pixel
       if samples_per_pixel is 'best':
-        samples_per_pixel = render_directories.ground_truth_samples_per_pixel()
-      assert render_directories.required_files_exist(samples_per_pixel, target_render_passes_usage)
+        samples_per_pixel = new_render_directories.ground_truth_samples_per_pixel()
+      assert new_render_directories.required_files_exist(samples_per_pixel, self.target_render_passes_usage)
       
-      render_directories_result.append(render_directories)
-    result[mode_name] = render_directories_result
+      self.render_directories.append(new_render_directories)
+  
+  def create(self):
+    tfrecords_directory = os.path.join(self.base_tfrecords_directory, self.name)
+    if not os.path.exists(tfrecords_directory):
+      os.makedirs(tfrecords_directory)
+  
+  def _compress(self, filename, delete_uncompressed=True):
+    gzip_filename = filename + '.gz'
+    original_file = open(filename, 'rb')
+    gzip_file = gzip.open(gzip_filename, 'wb')
+    gzip_file.writelines(original_file)
+    gzip_file.close()
+    original_file.close()
+    if delete_uncompressed:
+      os.remove(filename)
 
-  return result
 
 def main(parsed_arguments):
   try:
@@ -65,8 +79,7 @@ def main(parsed_arguments):
   except:
     print('Expected a valid json file as argument.')
   
-  mode_names_to_render_directories = mode_name_to_render_directories(parsed_json)
-  
+  base_render_directories = parsed_json['base_render_directories']
   base_tfrecords_directory = parsed_json['base_tfrecords_directory']
   mode_name_to_mode_settings = parsed_json['modes']
   
@@ -80,7 +93,20 @@ def main(parsed_arguments):
   target_render_passes_usage = RenderPassesUsage()
   target_render_passes_usage.__dict__ = target['features']
   
-
+  tfrecords_creators = []
+  for mode_name in mode_name_to_mode_settings:
+    mode_settings = mode_name_to_mode_settings[mode_name]
+    tfrecords_creator = TFRecordsCreator(
+        mode_name, base_tfrecords_directory, base_render_directories, mode_settings['render_directories'],
+        source_samples_per_pixel, source_render_passes_usage,
+        target_samples_per_pixel, target_render_passes_usage,
+        mode_settings['tiles_height_width'], mode_settings['tiles_per_tfrecords'])
+    tfrecords_creators.append(tfrecords_creator)
+    
+  for tfrecords_creator in tfrecords_creators:
+    tfrecords_creator.create()
+  
+  
 if __name__ == '__main__':
   parsed_arguments, unparsed = parser.parse_known_args()
   main(parsed_arguments)
